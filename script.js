31580f99134c7e137b3c6437662c674e5c932c3c
Patrick McMahon <pmcmahon14@gmail.com>
2018-06-24 22:41:44 -0700

    started finding prime factors

--- script.js
+++ script.js
@@ -4,3 +4,19 @@
 var this.2 = function two(num){
 
 };
+
+function enterKey(event) {
+    if (event.which == 13) {
+        findPrimeFactors();
+    }
+}
+
+function findPrimeFactors() {
+    num = ('#number').val();
+    n2 = 0;
+    n3 = 0;
+    if (num%2 = 0) {
+        n2 = n2 + 1;
+    } else
+
+}
